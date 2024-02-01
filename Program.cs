d2301bb87b25bdf0d08ea74dac57b1ac21d99f0f
Cine Student <Cine Student@CINEDIT02>
2024-01-31 20:49:04 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman !");
+            Console.WriteLine("Hey Mr Man I got the rope righ heer");
+            Console.WriteLine("ALL yee gots to do is put the wrong lettas so i can hang this BOY");
+            string[] Words = ["black", "Dark", " midnight train in geogria","Under the bed" ,"Shadow"];
+
+;
+        }
+
+    }
+}
